fb2ae3b46b457d44daa83f621694421b220ea45f
IlyaBobkin <103562689+IlyaBobkin@users.noreply.github.com>
2025-08-04 16:18:19 +0300

    2

--- app/src/main/java/com/example/passgen/presentation/generator/GeneratorScreen.kt
+++ app/src/main/java/com/example/passgen/presentation/generator/GeneratorScreen.kt
@@ -4,20 +4,30 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GeneratorScreen(viewModel: GeneratorScreenViewModel) {
+fun GeneratorScreen(viewModel: GeneratorScreenViewModel, navController: NavController) {
     val password = viewModel.generatedPassword
     val entropy = viewModel.entropy
 
@@ -28,49 +38,61 @@ fun GeneratorScreen(viewModel: GeneratorScreenViewModel) {
             uri?.let { viewModel.importPasswords(context, it) }
         }
     )
-    Button(onClick = { filePicker.launch(arrayOf("text/plain")) }) {
-        Text("Загрузить из файла")
-    }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text("Длина: ${viewModel.length}")
-        Slider(
-            value = viewModel.length.toFloat(),
-            onValueChange = { viewModel.length = it.toInt() },
-            valueRange = 6f..32f
-        )
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(checked = viewModel.useLetters, onCheckedChange = { viewModel.useLetters = it })
-            Text("Буквы")
-        }
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(checked = viewModel.useDigits, onCheckedChange = { viewModel.useDigits = it })
-            Text("Цифры")
-        }
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(checked = viewModel.useSymbols, onCheckedChange = { viewModel.useSymbols = it })
-            Text("Символы")
-        }
-
-        Button(
-            onClick = { viewModel.generate() },
-            modifier = Modifier.padding(vertical = 8.dp)
+    Scaffold(
+        topBar = { TopAppBar(title = {Text("Сгенерировать пароль")}, navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+            }
+        })}
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(15.dp)
         ) {
-            Text("Сгенерировать")
-        }
+            Button(onClick = { filePicker.launch(arrayOf("text/plain")) }) {
+                Text("Загрузить из файла")
+            }
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text("Длина: ${viewModel.length}")
+            Slider(
+                value = viewModel.length.toFloat(),
+                onValueChange = { viewModel.length = it.toInt() },
+                valueRange = 6f..32f
+            )
 
-        Text("Пароль: $password")
-        Text("Энтропия: ${"%.2f".format(entropy)}")
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(checked = viewModel.useLetters, onCheckedChange = { viewModel.useLetters = it })
+                Text("Буквы")
+            }
 
-        Button(onClick = { viewModel.savePassword() }) {
-            Text("Сохранить")
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(checked = viewModel.useDigits, onCheckedChange = { viewModel.useDigits = it })
+                Text("Цифры")
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(checked = viewModel.useSymbols, onCheckedChange = { viewModel.useSymbols = it })
+                Text("Символы")
+            }
+
+            Button(
+                onClick = { viewModel.generate() },
+                modifier = Modifier.padding(vertical = 8.dp),
+            ) {
+                Text("Сгенерировать")
+            }
+
+            Text("Пароль: $password")
+            Text("Энтропия: ${"%.2f".format(entropy)}")
+
+            Button(onClick = { viewModel.savePassword() }) {
+                Text("Сохранить")
+            }
         }
     }
+
+
 }
